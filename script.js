3a275f27dd06171d17eaa8c3cd8c2f59a07cc953
justinakoh <justina.y.koh@gmail.com>
2021-09-04 16:09:02 +1200

    Reset and start from (sort of scratch)

---
+++ script.js
@@ -0,0 +1,10 @@
+let districts = "https://github.com/deldersveld/topojson/blob/master/countries/new-zealand/new-zealand-districts.json";
+// let vaccinedata = "";
+
+
+let canvas = d3.select('#canvas')
+
+// Function which actually draws the map + importing the data
+let drawMap = () => {
+
+}
